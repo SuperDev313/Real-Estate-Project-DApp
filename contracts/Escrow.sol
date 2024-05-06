35b73a3cd1026b22fbaba063da0d719796caa60a
SuperDev313 <pablo313boss@gmail.com>
2024-05-05 17:21:43 -0700

    Escrow contract

---
+++ contracts/Escrow.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0
+
+interface IERC721 {
+    function transferFrom {
+        address _from,
+        address _to,
+        uint256 _id
+    } external;
+}
+
+contract Escrow {
+    address public nftAddress;
+    address payable public seller;
+    address public inspector;
+    address public lender;
+}
